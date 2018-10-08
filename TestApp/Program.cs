fc6c82d34aee952a4d0373f343ce0ac028dc8fa6
Mogens Heller Grabe <mookid8000@gmail.com>
2018-10-09 00:18:51 +0200

    add test app

---
+++ TestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Transport.InMem;
+using Serilog;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .CreateLogger();
+
+            using (var activator = new BuiltinHandlerActivator())
+            {
+                Configure.With(activator)
+                    .Logging(l => l.Serilog(Log.Logger))
+                    .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "logging-test"))
+                    .Start();
+
+                Console.WriteLine("Press ENTER to quit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
